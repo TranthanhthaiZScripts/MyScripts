20d03d944a7d58b32a4bebe07ef773f60417d841
TranthanhthaiZScripts <108127446+TranthanhthaiZScripts@users.noreply.github.com>
2023-03-31 10:59:16 +0700

    Update DoughV2Loader.lua

--- DoughV2Loader.lua
+++ DoughV2Loader.lua
@@ -1,4 +1,3 @@
--- Password:Tranthanhthaiz123
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
@@ -42,10 +41,10 @@ Button.MouseButton1Click:Connect(function()
     if checkCode(code) then
         
         ScreenGui:Destroy() -- delete the GUI
-        loadstring(game:HttpGet(("https://raw.githubusercontent.com/TranthanhthaiZScripts/TurboHubV2.5/main/Dough.lua"), true))()
+        loadstring(game:HttpGet(('https://raw.githubusercontent.com/TranthanhthaiZScripts/TurboHubV2.5/main/Dough.lua'), true))()
     else
         -- code 
         warn("Invalid code")
-        Players.LocalPlayer:Kick("Invalid code")
+        Players.LocalPlayer:Kick("Invalid Password")
     end
 end)
